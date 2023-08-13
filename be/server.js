cac692087353ccbf92933c9e6ab09d6313368cc0
Al Faza <m.alfazaatariq@gmail.com>
2023-08-13 12:49:33 +0700

    Move db connection to server.js

--- be/server.js
+++ be/server.js
@@ -1,6 +1,5 @@
 import Express from 'express';
 import { configDotenv } from 'dotenv';
-import connectToDB from './config/database.js';
 import videoRoutes from './routes/videos.js';
 import productRoutes from './routes/products.js';
 import authRoutes from './routes/auth.js';
@@ -10,10 +9,51 @@ import commentRoutes from './routes/comments.js';
 import { Server } from 'socket.io';
 import http from 'http';
 
+import mongoose from 'mongoose';
+import video from './models/videos.js';
+import product from './models/products.js';
+import comment from './models/comments.js';
+import user from './models/users.js';
+import populateVideos from './helpers/populateVideos.js';
+import populateProducts from './helpers/populateProducts.js';
+import populateComments from './helpers/populateComments.js';
+
 const app = Express();
 
 configDotenv();
-await connectToDB();
+
+try {
+  await mongoose.connect(process.env.DB_URL);
+  console.log('DB Connected!');
+
+  let videos = await video.count();
+  let products = await product.count();
+  // let comments = await comment.count();
+
+  // let products = await product.deleteMany({});
+  // let videos = await video.deleteMany({});
+  // let comments = await comment.deleteMany({});
+  // let users = await user.deleteMany({});
+
+  if (videos === 0) {
+    await populateVideos();
+    console.log('Videos Populated!');
+  }
+
+  if (products === 0) {
+    await populateProducts();
+    console.log('Products Populated!');
+  }
+
+  // if (comments === 0) {
+  //   await populateComments();
+  //   console.log('Comments Populated!');
+  // }
+} catch (error) {
+  console.log(error);
+}
+
+// connectToDB();
 
 app.use(Express.json());
 app.use(Express.static('public'));
